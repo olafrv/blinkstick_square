7fe8231911831a16d97c904af754dce18bd8852e
Olaf Reitmaier <olafrv@gmail.com>
2023-10-27 02:45:55 +0200

    htmlcov

--- coverage.ps1
+++ coverage.ps1
@@ -7,4 +7,10 @@ $SRC="blinkstick-python/blinkstick"
 
 coverage run --debug config --parallel-mode --rcfile=coverage_win32.rc test.py
 
+coverage combine --keep --append .\coverage\
+
 coverage report --show-missing $SRC\*.py
+
+# coverage html --rcfile=coverage_win32.rc -i --no-skip-covered  # Passed
+
+coverage html -i --no-skip-covered   # coverage_vs_other_blicksticks
--- coverage.sh
+++ coverage.sh
@@ -7,4 +7,10 @@ SRC="blinkstick-python/blinkstick"
 
 sudo coverage run --debug config --parallel-mode --rcfile=coverage_macm1.rc test.py
 
+coverage combine --keep ./coverage
+
 coverage report --show-missing $SRC/*.py
+
+# coverage html --rcfile=coverage_macm1.rc -i --no-skip-covered  # Passed
+
+coverage html -i --no-skip-covered  # coverage_vs_other_blicksticks
